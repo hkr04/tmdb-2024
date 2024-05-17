5b0ff3d9e52553c1f9dcaaf7ec11c7225b411c76
Sanqine <1078825799.com>
2024-05-17 16:35:21 +0800

    task 1 & 2 tested

---
+++ src/main/java/edu/whu/tmdb/query/operations/impl/DropImpl.java
@@ -0,0 +1,171 @@
+package edu.whu.tmdb.query.operations.impl;
+
+import edu.whu.tmdb.storage.memory.MemManager;
+import net.sf.jsqlparser.statement.Statement;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import edu.whu.tmdb.storage.memory.SystemTable.BiPointerTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.ClassTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.DeputyTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.ObjectTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.SwitchingTableItem;
+import edu.whu.tmdb.query.operations.Exception.TMDBException;
+import edu.whu.tmdb.query.operations.Drop;
+import edu.whu.tmdb.query.operations.utils.MemConnect;
+
+public class DropImpl implements Drop {
+
+    private MemConnect memConnect;
+
+    public DropImpl() {
+        this.memConnect = MemConnect.getInstance(MemManager.getInstance());
+    }
+
+    @Override
+    public boolean drop(Statement statement) throws TMDBException {
+        return execute((net.sf.jsqlparser.statement.drop.Drop) statement);
+    }
+
+    public boolean execute(net.sf.jsqlparser.statement.drop.Drop drop) throws TMDBException {
+        String tableName = drop.getName().getName();
+        int classId = memConnect.getClassId(tableName);
+        drop(classId);
+        return true;
+    }
+
+    public void drop(int classId) {
+        // TODO-task4
+        ArrayList<Integer> deputyClassIdList = new ArrayList<>();   // 存储该类对应所有代理类id
+
+        dropClassTable(classId);                            // 1.删除ClassTableItem
+        dropDeputyClassTable(classId, deputyClassIdList);   // 2.获取代理类id并在表中删除
+        dropBiPointerTable(classId);                        // 3.删除 源类/对象<->代理类/对象 的双向关系表
+        dropSwitchingTable(classId);                        // 4.删除switchingTable
+        dropObjectTable(classId);                           // 5.删除已创建的源类对象
+
+        // 6.递归删除代理类相关
+        // TODO-task4
+    }
+
+    /**
+     * 给定要删除的class id，删除系统表类表(class table)中的表项
+     * @param classId 要删除的表对应的id
+     */
+    private void dropClassTable(int classId) {
+        // TODO-task4
+        ArrayList<ClassTableItem> tempC=new ArrayList<>();
+        for (int i = 0; i <  MemConnect.getClassTableList().size(); i++) {
+            ClassTableItem classTableItem =MemConnect.getClassTableList().get(i);
+            if(classTableItem.classid==classId){
+                tempC.add(classTableItem);
+            }
+        }
+        for (ClassTableItem temp :
+                tempC) {
+            MemConnect.getClassTableList().remove(temp);
+        }
+
+        // List<ClassTableItem> classTableList = MemConnect.getClassTableList();
+
+        // // Remove all items from the class table that match the given classId
+        // boolean removed = classTableList.removeIf(item -> item.classid == classId);
+
+        // // Optionally, handle the case where no items were found (and thus nothing was removed)
+        // if (!removed) {
+        //     System.out.println("No entries found for classId: " + classId);
+        // }
+    }
+
+    /**
+     * 删除系统表中的deputy table，并获取class id对应源类的代理类id
+     * @param classId 源类id
+     * @param deputyClassIdList 作为返回值，源类对应的代理类id列表
+     */
+    private void dropDeputyClassTable(int classId, ArrayList<Integer> deputyClassIdList) {
+        ArrayList<DeputyTableItem> tempD=new ArrayList<>();
+        ArrayList<Integer> toDrop=new ArrayList<>();
+        for (int i = 0; i < MemConnect.getDeputyTableList().size(); i++) {
+            DeputyTableItem deputyTableItem = MemConnect.getDeputyTableList().get(i);
+            if(deputyTableItem.originid==classId){
+                toDrop.add(deputyTableItem.deputyid);
+                tempD.add(deputyTableItem);
+            }
+        }
+        for(DeputyTableItem temp: tempD){
+            MemConnect.getDeputyTableList().remove(temp);
+        }
+    }
+
+    /**
+     * 删除系统表中的BiPointerTable
+     * @param classId 源类id
+     */
+    private void dropBiPointerTable(int classId) {
+        ArrayList<BiPointerTableItem> tempB=new ArrayList<>();
+        for (int i = 0; i < MemConnect.getBiPointerTableList().size(); i++) {
+            BiPointerTableItem biPointerTableItem = MemConnect.getBiPointerTableList().get(i);
+            if(biPointerTableItem.objectid==classId || biPointerTableItem.deputyobjectid==classId){
+                tempB.add(biPointerTableItem);
+            }
+        }
+        for(BiPointerTableItem temp:tempB){
+            MemConnect.getBiPointerTableList().remove(temp);
+        }
+    }
+
+    /**
+     * 删除系统表中的SwitchingTable
+     * @param classId 源类id
+     */
+    private void dropSwitchingTable(int classId) {
+        // TODO-task4
+        ArrayList<SwitchingTableItem> tempS=new ArrayList<>();
+        for (int i = 0; i < MemConnect.getSwitchingTableList().size(); i++) {
+            SwitchingTableItem switchingTableItem = MemConnect.getSwitchingTableList().get(i);
+            if(switchingTableItem.oriId==classId || switchingTableItem.deputyId==classId){
+                tempS.add(switchingTableItem);
+            }
+        }
+        for(SwitchingTableItem temp:tempS){
+            MemConnect.getSwitchingTableList().remove(temp);
+        }
+    }
+
+    /**
+     * 删除源类具有的所有对象的列表
+     * @param classId 源类id
+     */
+    private void dropObjectTable(int classId) {
+        // TODO-task4
+        // 使用MemConnect.getObjectTableList().remove();
+        ArrayList<ObjectTableItem> tempT=new ArrayList<>();
+        ArrayList<Integer> toDrop=new ArrayList<>();
+        for (int i = 0; i < MemConnect.getDeputyTableList().size(); i++) {
+            DeputyTableItem deputyTableItem = MemConnect.getDeputyTableList().get(i);
+            if(deputyTableItem.originid==classId){
+                toDrop.add(deputyTableItem.deputyid);
+            }
+        }
+        for (int i = 0; i < MemConnect.getObjectTableList().size(); i++) {
+            ObjectTableItem objectTableItem = MemConnect.getObjectTableList().get(i);
+            if(objectTableItem.classid==classId ){
+                memConnect.DeleteTuple(objectTableItem.tupleid);
+                tempT.add(objectTableItem);
+            }
+        }
+        for(ObjectTableItem temp:tempT){
+            MemConnect.getObjectTableList().remove(temp);
+        }
+        if(toDrop.isEmpty()){
+            return;
+        }
+        for (int i = 0; i < toDrop.size(); i++) {
+            drop(toDrop.get(i));
+        }
+    }
+
+}
---
+++ src/main/java/edu/whu/tmdb/util/DbOperation.java
@@ -0,0 +1,148 @@
+package edu.whu.tmdb.util;
+
+import edu.whu.tmdb.query.operations.utils.MemConnect;
+import edu.whu.tmdb.query.operations.utils.SelectResult;
+import edu.whu.tmdb.storage.memory.SystemTable.BiPointerTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.ClassTable;
+import edu.whu.tmdb.storage.memory.SystemTable.ClassTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.DeputyTableItem;
+import edu.whu.tmdb.storage.memory.SystemTable.SwitchingTableItem;
+import edu.whu.tmdb.storage.memory.MemManager;
+import edu.whu.tmdb.storage.memory.Tuple;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbOperation {
+    /**
+     * 给定元组查询结果，输出查询表格
+     * @param result 查询语句的查询结果
+     */
+    public static void printResult(SelectResult result) {
+        // 输出表头信息
+        StringBuilder tableHeader = new StringBuilder("|");
+        for (int i = 0; i < result.getAttrname().length; i++) {
+            tableHeader.append(String.format("%-20s", result.getClassName()[i] + "." + result.getAttrname()[i])).append("|");
+        }
+        System.out.println(tableHeader);
+
+        // 输出元组信息
+        for (Tuple tuple : result.getTpl().tuplelist) {
+            StringBuilder data = new StringBuilder("|");
+            for (int i = 0; i < tuple.tuple.length; i++) {
+                data.append(String.format("%-20s", tuple.tuple[i].toString())).append("|");
+            }
+            System.out.println(data);
+        }
+    }
+
+    /**
+     * 删除数据库所有数据文件，即重置数据库
+     */
+    public static void resetDB() {
+        // 仓库路径
+        String repositoryPath = "D:\\cs\\JavaProject\\TMDB";
+
+        // 子目录路径
+        String sysPath = repositoryPath + File.separator + "data\\sys";
+        String logPath = repositoryPath + File.separator + "data\\log";
+        String levelPath = repositoryPath + File.separator + "data\\level";
+
+        List<String> filePath = new ArrayList<>();
+        filePath.add(sysPath);
+        filePath.add(logPath);
+        filePath.add(levelPath);
+
+        // 遍历删除文件
+        for (String path : filePath) {
+            File directory = new File(path);
+
+            // 检查目录是否存在
+            if (!directory.exists()) {
+                System.out.println("目录不存在：" + path);
+                return;
+            }
+
+            // 获取目录中的所有文件
+            File[] files = directory.listFiles();
+            if (files == null) { continue; }
+            for (File file : files) {
+                // 删除文件
+                if (file.delete()) {
+                    System.out.println("已删除文件：" + file.getAbsolutePath());
+                } else {
+                    System.out.println("无法删除文件：" + file.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    
+    public static void showClassTable() {
+        List<ClassTableItem> classTableLists = MemManager.classTable.classTableList;
+        
+        // 打印表头
+        System.out.printf("| %-20s | %-8s | %-20s | %-8s | %-14s |\n", "class name", "class id", "attribute name", "attribute id", "attribute type");
+        
+        // 遍历每个 ClassTableItem 实例，每个实例代表一个属性
+        for (ClassTableItem item : classTableLists) {
+            System.out.printf("| %-20s | %-8d | %-20s | %-12d | %-14s |\n", item.classname, item.classid, item.attrname, item.attrid, item.attrtype);
+        }
+    }
+    
+    public static void showDeputyTable() {
+        List<DeputyTableItem> deputyTableLists = MemManager.deputyTable.deputyTableList; 
+        // Print table header
+        System.out.printf("| %-20s | %-20s |\n", "origin class id", "deputy class id");
+
+        // Iterate over each item in the list and print formatted output
+        for (DeputyTableItem item : deputyTableLists) {
+            System.out.printf("| %-20s | %-20s |\n",
+                    item.originid,
+                    item.deputyid
+                    );
+        }
+    }
+
+
+    public static void showBiPointerTable() {
+        // TODO-task2
+        List<BiPointerTableItem> biPointerTableLists = MemManager.biPointerTable.biPointerTableList; 
+        // Print table header
+        System.out.printf("| %-20s | %-14s | %-12s | %-16s |\n","class id", "object id","deputy id","deputy object id");
+
+        // Iterate over each item in the list and print formatted output
+        for (BiPointerTableItem item : biPointerTableLists) {
+            System.out.printf("| %-20s | %-14s | %-14s | %-16s |\n",
+            item.classid,
+            item.objectid,
+            item.deputyid,
+            item.deputyobjectid
+            );
+        }
+    }
+
+
+    
+
+    public static void showSwitchingTable() {
+        List<SwitchingTableItem> switchingTableLists = MemManager.switchingTable.switchingTableList; // Assumed access to a list of switching table items
+
+        // Print table header
+        System.out.printf("| %-6s | %-11s | %-16s | %-9s | %-14s | %-17s |\n","origin class id", "origin attribute id", "origin attribute name", "deputy class id", "deputy attribute id", "deputy attribute name");
+
+        // Iterate over each item in the list and print formatted output
+        for (SwitchingTableItem item : switchingTableLists) {
+            System.out.printf("| %-6s | %-11s | %-16s | %-9s | %-14s | %-17s |\n",
+                    item.oriId,
+                    item.oriAttrid,
+                    item.oriAttr,
+                    item.deputyId,
+                    item.deputyAttrId,
+                    item.deputyAttr
+                    );
+        }
+    }
+    }
+
