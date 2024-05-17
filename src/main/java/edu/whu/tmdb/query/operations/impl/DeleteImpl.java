a51e134fadfa0ebee8bf46137724246eaa0be4d9
Rich <2350001636@qq.com>
2024-05-17 17:30:11 +0800

    task3 step4 未测试

--- src/main/java/edu/whu/tmdb/query/operations/impl/DeleteImpl.java
+++ src/main/java/edu/whu/tmdb/query/operations/impl/DeleteImpl.java
@@ -48,27 +48,60 @@ public class DeleteImpl implements Delete {
         delete(selectResult.getTpl());
     }
 
-    public void delete(TupleList tupleList) {
+    public void delete(TupleList tupleList) throws TMDBException {
         // TODO-task8
+        MemConnect memConnect = MemConnect.getInstance(MemManager.getInstance());
+        ArrayList<Integer> delete=new ArrayList<>();
         // 1.删除源类tuple和object table
+        // 使用MemConnect.getObjectTableList().remove();   // 删除对象表
 
-            // 使用MemConnect.getObjectTableList().remove();   // 删除对象表
+//        // 创建源类元组id列表、类id列表
+//        ArrayList<Integer> sourceTupleIdList = new ArrayList<Integer>();
+//        ArrayList<Integer> sourceClassIdList = new ArrayList<Integer>();
+//        for (int i = 0; i < tupleList.tuplelist.size(); i++)  {
+//            int sourceTupleId = tupleList.tuplelist.get(i).getTupleId();
+//            int sourceClassId = tupleList.tuplelist.get(i).classId;
+//            sourceTupleIdList.add(sourceTupleId);
+//            sourceClassIdList.add(sourceClassId);
+//        }
 
+        for (int i = 0; i < tupleList.tuplelist.size(); i++) {
+            // 获取源类元组ID
+            int sourceTupleId = tupleList.tuplelist.get(i).getTupleId();
+            int sourceClassId = tupleList.tuplelist.get(i).classId;
+            // 调用 memConnect.DeleteTuple 方法删除每个元组的ID
+            memConnect.DeleteTuple(sourceTupleId);
+            // 删除源类的对象表项
+            ObjectTableItem objectTableItem =new ObjectTableItem(sourceClassId,sourceTupleId);
+            // 删除源类的对象表
+            MemConnect.getObjectTableList().remove(objectTableItem);
+            delete.add(sourceTupleId);
+        }
 
+        ArrayList<Integer> deputyTupleIdList = new ArrayList<>();
 
         // 2.删除源类biPointerTable
-
-            // 使用MemConnect.getBiPointerTableList().remove();
-
+        // 使用MemConnect.getBiPointerTableList().remove();
+        for (int i = 0; i < MemConnect.getBiPointerTable().biPointerTableList.size(); i++) {
+            BiPointerTableItem biPointerTableItem = MemConnect.getBiPointerTable().biPointerTableList.get(i);
+            if(delete.contains(biPointerTableItem.objectid)){
+                deputyTupleIdList.add(biPointerTableItem.deputyobjectid);
+                MemConnect.getBiPointerTable().biPointerTableList.remove(biPointerTableItem);
+            }
+        }
+//        for (int i = 0; i < tupleList.tuplelist.size(); i++) {
+//            int sourceClassId = tupleList.tuplelist.get(i).classId;
+//            ArrayList<Integer> deputyTupleIdList = memConnect.getDeputyIdList(sourceClassId);
 
         // 3.根据biPointerTable递归删除代理类相关表
-        // if (deputyTupleIdList.isEmpty()) { return; }
-        // TupleList deputyTupleList = new TupleList();
-        // for (Integer deputyTupleId : deputyTupleIdList) {
-        //     Tuple tuple = memConnect.GetTuple(deputyTupleId);
-        //     deputyTupleList.addTuple(tuple);
-        // }
-        // delete(deputyTupleList);
+        if (deputyTupleIdList.isEmpty()) {return; }
+        // 创建代理类元组列表
+        TupleList deputyTupleList = new TupleList();
+        for (Integer deputyTupleId : deputyTupleIdList) {
+            Tuple deputyTuple = memConnect.GetTuple(deputyTupleId);
+            deputyTupleList.tuplelist.add(deputyTuple);
+        }
+        // 递归删除代理类相关表
+        delete(deputyTupleList);
     }
-
 }
